ce741c9b95b7f98be877fb08b14b308f37e73d01
Fatih GÜRSOY <fatihgursoy@ymail.com>
2016-03-14 19:17:18 +0200

    Size, Rating and Http scheme options added.

--- src/gravatar/picture.cr
+++ src/gravatar/picture.cr
@@ -1,7 +1,17 @@
 module Gravatar
 	class Picture
-		def self.profile_picture(email : String)
-			return "http://www.gravatar.com/avatar/#{Crypto::MD5.hex_digest(email)}"
+
+		HTTP_URL  = "http://www.gravatar.com/avatar/"
+		HTTPS_URL = "https://secure.gravatar.com/avatar/"
+
+		def self.profile_picture(email : String, size = 256 : Int, use_https = false : Bool, rating = 'g' : String)
+			url = HTTP_URL
+
+			if true == use_https
+				url = HTTPS_URL
+			end
+
+			return url + "#{Crypto::MD5.hex_digest(email.downcase)}" + sprintf("?s=%d&r=%s", size, rating)
 		end
 	end
 end
